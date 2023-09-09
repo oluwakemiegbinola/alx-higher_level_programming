5dc7fc835d2c8f65a6a718bc3595d89794d0a607
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-09-09 05:23:23 -0700

    updated

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
-    if 0 <= idx < len(my_list):
-        del(my_list[idx])
-    return (my_list)
+def delete_at (lst, position):
+    if position < 0 or position >= len(lst):
+        print("Invalid position")
+    else:
+        lst.pop(position)
+
+# Example usage:
+my_list = [1, 2, 3, 4, 5]
+delete_at (my_list, 2)  # Deletes the item at position 2
+print(my_list)  # Output: [1, 2, 4, 5]
