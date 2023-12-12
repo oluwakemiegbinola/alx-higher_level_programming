bfe387f542ad7c169bcbe81457e8b32ac796f5bd
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-12-12 19:47:36 +0530

    updated

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,22 +1,37 @@
-#!/usr/bin/node
 const fs = require('fs');
 
-const fileA = process.argv[2];
-const fileB = process.argv[3];
-const fileC = process.argv[4];
-
-if (
-  fs.existsSync(fileA) &&
-fs.statSync(fileA).isFile &&
-fs.existsSync(fileB) &&
-fs.statSync(fileB).isFile &&
-fileC !== undefined
-) {
-  const fileAContent = fs.readFileSync(fileA);
-  const fileBContent = fs.readFileSync(fileB);
-  const stream = fs.createWriteStream(fileC);
-
-  stream.write(fileAContent);
-  stream.write(fileBContent);
-  stream.end();
+if (process.argv.length !== 5) {
+  console.error('Usage: node 102-concat.js <file1> <file2> <destination>');
+  process.exit(1);
 }
+
+const [, , file1, file2, destination] = process.argv;
+
+// Read the content of the first file
+fs.readFile(file1, 'utf8', (err, data1) => {
+  if (err) {
+    console.error(`Error reading ${file1}: ${err.message}`);
+    process.exit(1);
+  }
+
+  // Read the content of the second file
+  fs.readFile(file2, 'utf8', (err, data2) => {
+    if (err) {
+      console.error(`Error reading ${file2}: ${err.message}`);
+      process.exit(1);
+    }
+
+    // Concatenate the content of the two files
+    const concatenatedContent = data1.trim() + '\n' + data2.trim();
+
+    // Write the concatenated content to the destination file
+    fs.writeFile(destination, concatenatedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to ${destination}: ${err.message}`);
+        process.exit(1);
+      }
+
+      console.log(`Concatenation successful! Check ${destination} for the result.`);
+    });
+  });
+});
