2b340688ec4a3d2a566aae6f39caf4372c88e3c9
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-12-13 10:32:48 +0530

    updated

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-const sortedArray = process.argv.slice(2).sort((a, b) => { return a - b; });
-const secondToLast = sortedArray[sortedArray.length - 2];
-console.log(isNaN(secondToLast) ? 0 : secondToLast);
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
