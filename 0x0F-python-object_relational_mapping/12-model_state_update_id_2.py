d2dc53844fca62321249e0e2fb643655eb0953b3
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-12-15 18:24:11 -0800

    task12

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,25 +1,27 @@
 #!/usr/bin/python3
-"""Module that updates the name of a state in a\
-        MySQL database using SQLAlchemy."""
-import sys
+"""
+This script changes the name of a State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-    # Create a session factory
+    """
+    Updates a State object on the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
-    # Create a session object
+
     session = Session()
 
-    # Retrieve the state with ID 2 from the database
-    state = session.query(State).filter_by(id=2).first()
-    # Update the name of the state to "New Mexico"
-    state.name = "New Mexico"
-    # Commit the session to persist the changes
+    ari_state = session.query(State).filter(State.id == '2').first()
+    ari_state.name = 'New Mexico'
     session.commit()
-
+    session.close()
