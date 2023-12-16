1096b1e5007de420e9b8b7a86479c486ead4a3e1
oluwakemiegbinola <oluwakemiegbinola@gmail.com>
2023-12-15 18:19:59 -0800

    updated

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python3
-"""Module that searches for a state in a MySQL database using SQLAlchemy."""
-import sys
+"""
+This script prints the State object id
+with the name passed as argument
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # Create a session factory
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
-    # Create a session object
     session = Session()
+    instance = session.query(State).filter(State.name == argv[4]).first()
 
-    # Search for the specified state in the database
-    found = False
-    for state in session.query(State):
-        if state.name == sys.argv[4]:
-            print("{}".format(state.id))
-            found = True
-            break
-
-    # Print a message if the state is not found
-    if found is False:
-        print("Not found")
-
+    if instance is None:
+        print('Not found')
+    else:
+        print('{0}'.format(instance.id))
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,29 @@
 #!/usr/bin/python3
-"""Module that retrieves and prints the first state\
-        from a MySQL database using SQLAlchemy."""
-import sys
+"""
+This script prints the first State object
+from the database `hbtn_0e_6_usa`.
+"""
+
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import State
 
 if __name__ == "__main__":
-    # Create the SQLAlchemy engine using the provided MySQL credentials
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    """
+    Access to the database and get a state
+    from the database.
+    """
 
-    # Create a session factory
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
     Session = sessionmaker(bind=engine)
 
-    # Create a session object
     session = Session()
+    instance = session.query(State).order_by(State.id).first()
 
-    # Retrieve the first state from the database and print its ID and name
-    state = session.query(State).order_by(State.id).first()
-    if state is None:
-        print("Nothing")
+    if instance is None:
+        print('Nothing')
     else:
-        print("{}: {}".format(state.id, state.name))
-
+        print('{0}: {1}'.format(instance.id, instance.name))
